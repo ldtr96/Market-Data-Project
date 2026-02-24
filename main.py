7b375d5775e76dd05289559822b3deca00c01f95
Long <129347909+ldtr96@users.noreply.github.com>
2026-02-24 00:18:21 -0500

    Added argparse to parse ticker from CLI. Added main function as entry point.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlite3
 from IngestTransform import get_price_history, calculate_metrics
 from load_db import load_to_sqlite
+import argparse
 
 #This shall be the main file where the orchestration and run should occur.
 
@@ -11,7 +12,25 @@ from load_db import load_to_sqlite
 def orchestrate_data(ticker):
     t_info = get_price_history(ticker)
     t_info = calculate_metrics(t_info,ticker)
-    t_info = load_to_sqlite(t_info)
+    #t_info = load_to_sqlite(t_info)
     return t_info
 
 #Main logic will go here
+
+def main():
+    #Creates a required argument to be passed in via command line.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ticker",
+        help="The ticker of the position you want to import.",
+        required=True
+        )
+    args = parser.parse_args()
+    ticker = args.ticker
+    #Orchestration function using the argument passed in with the help of argparse.
+    t_info = orchestrate_data(ticker)
+
+    return t_info
+
+if __name__ == "__main__":
+    print(main())
