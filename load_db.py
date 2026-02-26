4bc51734575500583ede93de96ca610bf76631ec
Long <129347909+ldtr96@users.noreply.github.com>
2026-02-25 21:02:32 -0500

    Added logic to filter for new rows before adding to DB.

--- load_db.py
+++ load_db.py
@@ -4,7 +4,12 @@ import pandas as pd
 #Loads dataframe from the MarketData-Modular file into SQLite DB market_data.db
 def load_to_sqlite(df):
     con = sqlite3.connect("market_data.db")
-    df.to_sql("ticker_price_data",con, if_exists="append", index = False)
+    
+    existing_data = pd.read_sql('SELECT Ticker, Date FROM ticker_price_data',con)
+
+    df_new = df.merge(existing_data,how="left",on=["Ticker","Date"],indicator=True)
+    df_new = df_new[df_new["_merge"]=="left_only"].drop(columns=["_merge"])
+    df_new.to_sql("ticker_price_data",con, if_exists="append", index = False)
 
     # Verify how many rows are in the DB now.
     cur = con.cursor()
