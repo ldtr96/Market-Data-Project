5fdd88217a5e313c146039ddf8ef1bd7ec1a59ce
Long <129347909+ldtr96@users.noreply.github.com>
2026-03-14 15:34:51 -0400

    Added a search input, date, and table to interact with FastAPI endpoint.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,88 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import axios from 'axios'
+import './App.css'
 
 function App() {
   
 const [data, setData] = useState(null);
 const [isLoading, setIsLoading] = useState(true);
-const [ticker, setTicker] = useState('AAPL'); /* AAPL is the default here */
+const [ticker, setTicker] = useState('META'); /* AAPL is the default here */
+const[searchTicker, setSearchTicker] = useState('META')
+const[startDate, setStartDate] = useState('')
+const[endDate, setEndDate] = useState('')
+const [searchTrigger, setSearchTrigger] = useState(0)
 
 useEffect(()=> {
-  axios.get(`http://127.0.0.1:8000/data/${AAPL}`)
+  axios.get(`http://127.0.0.1:8000/data/${searchTicker}?start_date=${startDate}&end_date=${endDate}`)
   .then(response=>{
-    setData(response.data)
+    console.log(response.data.data)
+    setData(response.data.data)
     setIsLoading(false)
   })
 
   .catch(error=>{
     console.log(error)
   })
-}, [ticker])
+}, [searchTicker,searchTrigger])
 
 if (isLoading){
   return <p>Loading...</p>
 }
 
-  
   return (
     <>
-      <div>
-        <h1>{ticker}Market Data</h1>
+      <div className="container">
+      <div className="search-bar">
+        <h1>{ticker} Market Data</h1>
         <input 
           value={ticker}
-          onChange={
-            e=> {setTicker(e.target.value)}
-          }
+          onChange={e=> {setTicker(e.target.value)}}
         />
+
+        <input
+          type="date"
+          value={startDate}
+          onChange={e => setStartDate(e.target.value)}
+          />
+        
+        <input
+          type="date"
+          value={endDate}
+          onChange={e => setEndDate(e.target.value)}
+          />
+
+        <button onClick={()=>{
+          setSearchTicker(ticker)
+          setSearchTrigger(prev => prev + 1)
+        }}>
+          Search
+        </button>
+      </div>
+
+      <div className= "data-table">
+        <table>
+          <thead className="header">
+            <tr>
+              <th>Date</th>
+              <th>Ticker</th>
+              <th>Open</th>
+              <th>Close</th>
+              <th>Intraday Difference</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row,i)=> (
+              <tr key = {i}>
+                <td>{row.Date}</td>
+                <td>{row.Ticker}</td>
+                <td>{parseFloat(row.Open).toFixed(2)}</td>
+                <td>{parseFloat(row.Close).toFixed(2)}</td>
+                <td style={{color: row.Intraday_Difference >= 0 ? 'green' : 'red'}}>{parseFloat(row.Intraday_Difference).toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       </div>
     </>
   )
